8f10005aa7e0456e9e45269971d75bea18b50c7e
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-06 15:37:46 +0300

    Added on Root page logic that open/close menu from call button

--- src/pages/RootPage/RootPage.js
+++ src/pages/RootPage/RootPage.js
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import './RootPage.css';
 import PhoneIcon from '../../components/PhoneIcon';
 import Menu from '../../components/Menu/Menu';
 import Button from '../../components/Button';
 import ImageWithText from '../../components/ImageWithText';
+import Dropdown from '../../layouts/Dropdown';
+import Icon from '../../components/Icon';
+import Hint from '../../components/Hint';
 import _ from 'lodash';
 import cn from 'classnames';
+import {useOnClickOutside} from '../../helpers/hooks';
 
 const USER_TEST_DATA = [
   {id: 1, name: 'Denise', location: 'New York, NY', imgSrc: 'images/test_user.png'},
@@ -25,16 +29,41 @@ const USER_TEST_DATA = [
 
 const RootPage = () => {
 
+  const refDropdown = useRef(null);
+
+  const [showPhoneIconMenu, setShowPhoneIconMenu] = useState(false);
+  useOnClickOutside(refDropdown, () => setShowPhoneIconMenu(false));
+
   // const [selectedUser, setSelectedUser] = useState(_.first(USER_TEST_DATA).id);
   //
   // const handlerSetSelectedUser = () => {
   //
   // }
 
+  const handlerOnClickUser = () => {
+
+  }
+
+  const handlerOnClickPhoneIcon = () => {
+    setShowPhoneIconMenu(!showPhoneIconMenu)
+  }
+
   return (
     <div>
       <div className='top-panel'>
-        <PhoneIcon/>
+        <PhoneIcon onClick={handlerOnClickPhoneIcon} />
+        {
+          showPhoneIconMenu ? (
+            <Dropdown ref={refDropdown} className='phone-icon-menu-container'>
+              <Icon text='Call Approvals' imgSrc='images/UserApprovals.png'/>
+              <Icon text='Call Log' imgSrc='images/UserApprovals.png'/>
+              <Icon text='Call Purchase' imgSrc='images/UserApprovals.png'/>
+              <Icon text='Call Availability' imgSrc='images/UserApprovals.png'/>
+              <div style={{position: 'relative'}}>
+                <Hint style={{position: 'absolute', top: 10, right: -15}} text='Help' />
+              </div>
+            </Dropdown>) : null
+        }
         <Menu/>
       </div>
       <div className='grid-of-users-container'>
@@ -42,7 +71,7 @@ const RootPage = () => {
           _.map(USER_TEST_DATA, u => {
             return (
               <div className={cn('user-item-container')} key={u.id}>
-                <ImageWithText imgSrc={u.imgSrc} title={u.name} subtitle={u.location} />
+                <ImageWithText onClick={handlerOnClickUser} imgSrc={u.imgSrc} title={u.name} subtitle={u.location}/>
                 <div className='user-item-button-container'>
                   <Button label='Hide' style='gray'/>
                   <Button label='Like' style='green'/>
