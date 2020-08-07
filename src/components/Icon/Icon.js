b22af8dacfd4ae91cf5ef2028e16507435f37d9c
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-07 10:59:08 +0300

    Refactored Icon component (passed className to container and styling it)

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Icon.css';
+import cn from 'classnames';
 
-const Icon = ({text, alt, imgSrc, ...args}) => {
+const Icon = ({text, alt, imgSrc, className,  ...args}) => {
    return (
-     <div className='icon-container' {...args}>
+     <div className={cn('icon-container', className)} {...args}>
        <img className='icon-element' src={imgSrc} alt={alt}/>
        <span className='icon-element-text' >{text}</span>
      </div>
