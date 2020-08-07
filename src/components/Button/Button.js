b4448c7fae179a91687e6a847975f5300403364f
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-07 10:56:58 +0300

    Improved button component (added left icon and styling it)

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -7,11 +7,14 @@ const STYLE_OF_BUTTONS = {
   'green': 'green-button',
 }
 
-const Button = ({onClick, style, label}) => {
+const Button = ({onClick, style, className, label, leftIcon}) => {
   return (
-    <button onClick={onClick} className={cn(STYLE_OF_BUTTONS[style], 'common-button-style')}>
-      {label}
-    </button>
+    <div className={cn('button-container', className)}>
+      {leftIcon}
+      <button onClick={onClick} className={cn(STYLE_OF_BUTTONS[style], 'common-button-style')}>
+        {label}
+      </button>
+    </div>
   )
 }
 
