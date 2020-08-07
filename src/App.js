d7a1def4862d93bfeadbf20007866475d0d0f622
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-07 11:05:47 +0300

    Added browserRouter and implemented this on App.js (Added two routes: RootPage and UserPage)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import RootPage from './pages/RootPage';
+import RootPage from './pages/RootPage'
+import UserPage from './pages/UserPage';
 import './App.css';
+import {
+  Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {customHistory} from './helpers/history';
 
 function App() {
   return (
     <div className="App">
-      <RootPage />
+      <Router history={customHistory}>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <RootPage/>
+            </Route>
+            <Route path="/user/:id">
+              <UserPage/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
