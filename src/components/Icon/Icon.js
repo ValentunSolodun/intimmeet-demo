7c4ee58dc4c86b92f49764eb1d8458c3b831d3e6
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-11 10:50:35 +0300

    Improved Icon component(added ability for custom class)

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './Icon.css';
 import cn from 'classnames';
 
-const Icon = ({text, alt, imgSrc, className,  ...args}) => {
+const Icon = ({text, alt, imgSrc, className, classNameImg,  ...args}) => {
    return (
      <div className={cn('icon-container', className)} {...args}>
-       <img className='icon-element' src={imgSrc} alt={alt}/>
+       <img className={cn('icon-element', classNameImg)} src={imgSrc} alt={alt}/>
        <span className='icon-element-text' >{text}</span>
      </div>
    )
