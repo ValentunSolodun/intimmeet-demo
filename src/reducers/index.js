830dfa737c98ae6da187d1c2ed937a6ac799219e
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-11 11:58:04 +0300

    Added user reducer to combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import UsersReducer from './users.reducer';
+import UserReducer from './user.reducer';
 
 export default combineReducers({
   users: UsersReducer,
+  user: UserReducer
 });
