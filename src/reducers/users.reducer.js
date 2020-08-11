a36d3feb3e782a3af1f4a63c3431c8a4b110dae6
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-11 11:51:20 +0300

    Customized users.reducer

--- src/reducers/users.reducer.js
+++ src/reducers/users.reducer.js
@@ -1,1 +1,31 @@
+import {GET_USERS_SUCCESS, GET_USERS_FAILED} from '../actions';
+
+const initialState = {
+  data: [],
+  errors: null,
+  isLoading: true,
+};
+
+const users = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_USERS_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        data: [action.payload]
+      }
+    }
+    case GET_USERS_FAILED: {
+      return {
+        ...state,
+        isLoading: false,
+        errors: action.payload
+      }
+    }
+    default:
+      return state;
+  }
+}
+export default users;
+
 
