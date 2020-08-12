ee191ee99f4f5f8090add452bafa312e337758f0
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-12 19:51:09 +0300

    Remade UserPage (now it has IntemmeetButton as root element of library)

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -11,6 +11,18 @@ import Hint from '../../components/Hint';
 import Dropdown from '../../components/Dropdown';
 import {useOnClickOutside} from '../../helpers/hooks';
 import Loading from '../../components/Loading';
+import _ from 'lodash';
+import IntemmeetButton from '../../components/IntemmeetButton';
+
+// const MENU_ITEM = [
+//   {id: 1, name: 'Call', icon: '/images/icons/approvals.svg'},
+//   {id: 1, name: 'Video Call', icon: '/images/icons/call_logs.svg'},
+//   {id: 1, name: 'Speed Date', icon: '/images/icons/speed_date.svg'},
+//   {id: 1, name: 'Images', icon: '/images/icons/images.svg'},
+//   {id: 1, name: 'Passion & Truth Detector', icon: '/images/icons/credit_manager.svg'},
+//   {id: 1, name: 'Settings', icon: '/images/icons/settings.svg'},
+// ];
+
 
 // const USER_TEST_DATA = {
 //   id: 1,
@@ -22,9 +34,13 @@ import Loading from '../../components/Loading';
 
 const UserPage = ({renderData, getUser}) => {
   const {user, isLoading, errors} = renderData;
-  const refDropdown = useRef(null);
-  const [showPhoneIconMenu, setShowPhoneIconMenu] = useState(false);
-  useOnClickOutside(refDropdown, () => setShowPhoneIconMenu(false), ['user-icon-go-call-img']);
+  // const refDropdown = useRef(null);
+  // const [showPhoneIconMenu, setShowPhoneIconMenu] = useState(false);
+  // const [selectedMenuItem, setSelectedMenuItem] = useState('');
+  // useOnClickOutside(refDropdown, () => setShowPhoneIconMenu(false), ['user-icon-go-call-img']);
+  // const handlerOnClickMenu = (key) => {
+  //   setSelectedMenuItem(key);
+  // };
 
   useEffect(() => {
     getUser();
@@ -34,38 +50,20 @@ const UserPage = ({renderData, getUser}) => {
     customHistory.goBack();
   };
 
-  const handlerOnClickPhoneIcon = () => {
-    setShowPhoneIconMenu(!showPhoneIconMenu)
-  };
+  // const handlerOnClickPhoneIcon = () => {
+  //   setShowPhoneIconMenu(!showPhoneIconMenu)
+  // };
 
   return (
     <div className='user-page-container'>
       {
         isLoading ? <Loading/> : null
       }
+      <IntemmeetButton classNameOverlayDropdown='user-page-container__dropdown-overlay' user={user} />
+      <Icon className='user-icon-add-to-favourite' imgSrc='/images/icons/to_favourite.svg'/>
       <div className='user-avatar-container'>
         <div className='user-page-avatar-container'>
-          <img className='user-avatar-item' src="/images/test_user.png" alt=""/>
-          <Icon className='user-icon-add-to-favourite' imgSrc='/images/heart.png' text='Heart'/>
-          <Icon onClick={handlerOnClickPhoneIcon}
-                className='user-icon-go-call'
-                classNameImg='user-icon-go-call-img'
-                imgSrc={user.imgSrc}
-                text='Heart'
-          />
-          {
-            showPhoneIconMenu ? (
-              <Dropdown ref={refDropdown} className='user-icon-go-call-dropdown phone-icon-menu-container '>
-                <Icon className='phone-icon-menu-item' text='Call' imgSrc='/images/UserApprovals.png'/>
-                <Icon className='phone-icon-menu-item' text='Video call' imgSrc='/images/UserApprovals.png'/>
-                <Icon className='phone-icon-menu-item' text='Game call' imgSrc='/images/UserApprovals.png'/>
-                <Icon className='phone-icon-menu-item' text='Call Availability' imgSrc='/images/UserApprovals.png'/>
-                <div style={{position: 'relative'}}>
-                  <Hint style={{position: 'absolute', top: 10, right: -15}} text='Help'/>
-                </div>
-              </Dropdown>) : null
-          }
-
+          <img className='user-avatar-item' src={user.imgSrc} alt=""/>
         </div>
         <div className='user-name-container'>
           {user.name}
