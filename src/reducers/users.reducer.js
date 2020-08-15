7757470ef11c322e470519b000530ef5fb3b0b56
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-15 19:53:14 +0300

    Added case GET_USERS_IDS_SUCCESS to users.reducer

--- src/reducers/users.reducer.js
+++ src/reducers/users.reducer.js
@@ -1,4 +1,5 @@
-import {GET_USERS_SUCCESS, GET_USERS_FAILED} from '../actions';
+import {GET_USERS_SUCCESS, GET_USERS_FAILED, GET_USERS_IDS_SUCCESS} from '../actions';
+import _ from 'lodash';
 
 const initialState = {
   data: [],
@@ -8,6 +9,16 @@ const initialState = {
 
 const users = (state = initialState, action) => {
   switch (action.type) {
+    case GET_USERS_IDS_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        data: [..._.map(state.data, d => ({
+          ...d,
+          guId: _.get(_.find(action.payload, ['targetId', d.id]), 'guId') || null
+        }))]
+      }
+    }
     case GET_USERS_SUCCESS: {
       return {
         ...state,
