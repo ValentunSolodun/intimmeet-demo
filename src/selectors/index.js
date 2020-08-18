f3659987b362aa839e2b13f803baf817de608a20
ValentunSolodun <valentun.solodun@gmail.com>
2020-08-18 10:14:46 +0300

    Added selectors to get data from approvals and callLog selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -8,6 +8,21 @@ export const getUsersSelector = createSelector(
   (data, isLoading, errors) => ({users: data, isLoading, errors})
 )
 
+export const getCallLogSelector = createSelector(
+  state => state.callLog.data,
+  state => state.callLog.isLoading,
+  state => state.callLog.errors,
+  (data, isLoading, errors) => ({callLog: data, isLoading, errors})
+)
+
+export const getApprovalsSelector = createSelector(
+  state => _.filter(state.approvals.data, d => d.approving_user_id === JSON.parse(localStorage.getItem('user')).id),
+  state => _.filter(state.approvals.data, d => d.approved_user_id === JSON.parse(localStorage.getItem('user')).id),
+  state => state.approvals.isLoading,
+  state => state.approvals.errors,
+  (myApproved, approvedMy, data, isLoading, errors) => ({approvals: {myApproved, approvedMy}, isLoading, errors})
+)
+
 export const getUserSelector = createSelector(
   state => state.user.data,
   state => state.user.isLoading,
